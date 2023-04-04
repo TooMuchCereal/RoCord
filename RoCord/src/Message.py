42232728cbd8182e5a26cc5016ded582f014d767
Corez <124507028+TooMuchCereal@users.noreply.github.com>
2023-04-04 08:24:24 -0700

    Add files via upload

---
+++ RoCord/src/Message.py
@@ -0,0 +1,2 @@
+Welcome = 'Hello {} Im Corez_Bot, Programmed By Corez_Dev#3095 For A Neat GITHUB Repos. Im Here To Assist As Pleased!'
+Group = '{} Is A Roblox Group Made By {} That Has {} Members And {}'
---
+++ RoCord/src/Request.py
@@ -0,0 +1,51 @@
+#---IMPORTS---#
+
+import discord
+from discord.ext import commands
+
+import robloxpy
+from Message import *
+
+#---BOT IS READY---#
+
+client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+#--Get A Small Hello From Bot--#
+
+@client.command()
+async def Hey(ctx):
+      await ctx.send(Welcome.format(ctx.message.author.mention))
+
+#--Get A Groups Info--#    
+
+@client.command()
+async def GetGroupInfo(ctx):
+    
+    Context_Message = ctx.message.content.split(" ")[1]
+    
+    try:
+        Group_Description = robloxpy.Group.External.GetDescription(Context_Message)
+        Group_Count = robloxpy.Group.External.GetMemberCount(Context_Message)
+
+        Group_Name = robloxpy.Group.External.GetName(Context_Message)
+        Group_Owner = robloxpy.Group.External.GetOwner(Context_Message)
+
+
+
+        await ctx.send(Group.format(Group_Name, Group_Owner, Group_Count, Group_Description))    
+    except:
+        await ctx.send("Sorry Couldnt Get Group Info")
+@client.command()
+async def GetUserImage(ctx):
+    
+    Context_Message = ctx.message.content.split(" ")[1]
+    
+    try:
+        Image = robloxpy.User.External.GetHeadshot(Context_Message)
+        Embed = discord.embed(title=f'{ctx.author.mention} Has Requested An Image')
+
+        Embed.add_field(name='Content', value=Image, inline=False)
+        await ctx.send(embed=Embed)
+    except:
+        pass
+client.run("MTA5Mjc0MzYxODMyMzgwODI4Ng.GFA_HQ.s2pn8VYKKyJ8FgJJ8GihpWHPFe5i_jQDLm9gjA")
